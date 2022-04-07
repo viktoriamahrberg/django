76ccc22a05088132e44c931c27cc14229e870f58
Vikmah <viktoria.mahrberg@gmail.com>
2022-04-07 12:19:05 +0000

    Set up environment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4d)#$-bhwl(+@5v_$n9=*)rzzr*b3xb7&!ov8ryl&&nxf+g0l8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4d)#$-bhwl(+@5v_$n9=*)rzzr*b3xb7&!ov8ryl&&nxf+g0l8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-todo-app-vik.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -75,13 +75,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-    'default': dj_database_url.parse('postgres://mwmkxkshirveix:9d196f03f230666e29fc7530e8c59fe11e2de4a966fa00ea85b6a0cdb1a6b646@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d1lcsbdcr4e1ac')            
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))            
 }
 
 
